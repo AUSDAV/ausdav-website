3564232749adf4b35fa248630e4025bad2bd48ea
anushan1508 <anushan1508@gmail.com>
2021-06-12 02:17:25 +0530

    Sidebar design

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { 
+  SidebarContainer, 
+  Icon, 
+  CloseIcon, 
+  SidebarWrapper, 
+  SidebarMenu, 
+  SidebarLink, 
+  SidebarRoute, 
+  SideBtnWrap 
+} from './SidebarElements'
+
+const Sidebar = ({isOpen,toggle}) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='project' onClick={toggle}>Projects</SidebarLink>
+          <SidebarLink to='history' onClick={toggle}>History</SidebarLink>
+          <SidebarLink to='structure' onClick={toggle}>Structure</SidebarLink>
+          <SidebarLink to='contact' onClick={toggle}>Contact</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to='/result'>Examination Results</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
